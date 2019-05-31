7ca8af23433efe9731b47aeb5f1cf012ac011409
Vimpil <vimpil@ya.ru>
2019-06-01 02:33:47 +0300

    css and js files added

--- wp_plugin_example1.php
+++ wp_plugin_example1.php
@@ -8,9 +8,16 @@ defined('ABSPATH') or die('no wordpress path found');
 class wp_plugin_example1
 {
 
+
     function __construct(){
         add_action( 'init', array ( $this, 'custom_post_type' ));
     }
+
+    function register() {
+        // add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );   
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );   
+    }
+
     // function __construct($string){
     //     echo $string;
     // }
@@ -25,16 +32,21 @@ class wp_plugin_example1
     }
 
 
-    function custom_post_type() {
+    protected function custom_post_type() {
         register_post_type( 'example_post1', ['public' => true, 'label' => 'Books'] );
     }
 
-    
+    function enqueue() {
+        wp_enqueue_style( 'wp_plugin_example1', plugins_url('/assets/mystyle.css', __FILE__));
+        wp_enqueue_script( 'wp_plugin_example1', plugins_url('/assets/myscript.js', __FILE__));
+    }
+
 
 }
 
-if (class_exists( 'wp_plugin_example1' ) ) {
-    $wp_plugin_example1 = new Wp_plugin_example1('something');
+if ( class_exists( 'wp_plugin_example1' ) ) {
+    $wp_plugin_example1 = new Wp_plugin_example1();
+    $wp_plugin_example1->register();
 }
 
 // activation
